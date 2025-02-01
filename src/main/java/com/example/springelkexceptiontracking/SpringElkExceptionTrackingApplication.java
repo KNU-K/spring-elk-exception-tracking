1176c9b65b44a2ee00ca0e9df15634ccc279803b
KNU-K <knukang334@gmail.com>
2025-02-02 06:36:15 +0900

    feat : logback-encoder를 통해서 info level 이상의 로그를 logs 경로의 application.log 에 로그백 되도록 구성

---
+++ src/main/java/com/example/springelkexceptiontracking/SpringElkExceptionTrackingApplication.java
@@ -0,0 +1,13 @@
+package com.example.springelkexceptiontracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringElkExceptionTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringElkExceptionTrackingApplication.class, args);
+	}
+
+}
